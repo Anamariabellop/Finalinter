7bad38bc7d2d51a124a0278879a0978f121e76bd
Anamariabellop <am.bello@uniandes.edu.co>
2019-07-22 19:43:02 +0000

    ej 2 corregido

--- Ejercicio2.py
+++ Ejercicio2.py
@@ -6,11 +6,16 @@ import numpy as np
 x = np.int_(np.random.random(100)*10000)
 print(x)
 
+numdivi=0
 
 for i in range(len(x)):
     if( x[i]%2!=0 and x[i]>100):
         print(x[i])
-    else:
-        break
+        
+        for j in range(1,i):
+            if(x[i]%x[j]==0):
+                numdivi+=1
+        if(numdivi<=2):
+            break
         
         
